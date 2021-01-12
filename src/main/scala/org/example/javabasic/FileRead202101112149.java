539b8d9a0533788cae97f26ad6a78e7d6c06ebf3
yangjichun182 <390835164@qq.com>
2021-01-12 20:30:20 +0800

    数据写入文件里面demo

--- src/main/scala/org/example/javabasic/FileRead202101112149.java
+++ src/main/scala/org/example/javabasic/FileRead202101112149.java
@@ -1,8 +1,20 @@
 package org.example.javabasic;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- *
+ * @author : chunji.yang
+ * @date   : 2021-01-11 22:27
+ * @remark : 实现将数据写到文件里面
  */
 public class FileRead202101112149 {
-
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("F:\\workspace\\Flink3\\src\\main\\resources\\a.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("123");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
 }
