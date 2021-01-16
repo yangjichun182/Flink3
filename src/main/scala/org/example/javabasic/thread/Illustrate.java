8768977fdcfc0bd64afa77860171d31f2a65a57a
yangjichun182 <390835164@qq.com>
2021-01-16 21:11:02 +0800

    src\main\scala\org\example\javabasic\thread\ 这个目录下的类说明

--- src/main/scala/org/example/javabasic/thread/Illustrate.java
+++ src/main/scala/org/example/javabasic/thread/Illustrate.java
@@ -5,7 +5,7 @@ import org.example.javabasic.thread.ThreadTest202101162019;
 /**
  * @author : chunji.yang
  * @date   : 2021-01-16 21:06
- * @remark :src\main\scala\org\example\javabasic\algorithm 这个目录下的类说明
+ * @remark :src\main\scala\org\example\javabasic\thread\ 这个目录下的类说明
  */
 public class Illustrate {
 }
