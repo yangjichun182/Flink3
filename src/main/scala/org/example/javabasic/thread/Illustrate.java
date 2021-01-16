aef4969b267386ec9853ddf915f83e0c96ded7c8
yangjichun182 <390835164@qq.com>
2021-01-16 21:09:11 +0800

    src\main\scala\org\example\javabasic\thread\ 这个目录下的类说明

---
+++ src/main/scala/org/example/javabasic/thread/Illustrate.java
@@ -0,0 +1,11 @@
+package org.example.javabasic.thread;
+
+//Java线程技术基础认识
+import org.example.javabasic.thread.ThreadTest202101162019;
+/**
+ * @author : chunji.yang
+ * @date   : 2021-01-16 21:06
+ * @remark :src\main\scala\org\example\javabasic\algorithm 这个目录下的类说明
+ */
+public class Illustrate {
+}
