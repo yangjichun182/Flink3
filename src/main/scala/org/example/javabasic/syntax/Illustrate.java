5ecace56ce658ce805c8cefefbc1b62537b17db0
yangjichun182 <390835164@qq.com>
2021-01-18 20:47:27 +0800

    org.example.javabasic.syntax 包下的类功能列表

---
+++ src/main/scala/org/example/javabasic/syntax/Illustrate.java
@@ -0,0 +1,9 @@
+package org.example.javabasic.syntax;
+
+/**
+ * @author : chunji.yang
+ * @date : 2021-01-18 20:45
+ * @remark : org.example.javabasic.syntax 包下的类功能列表
+ */
+public class Illustrate {
+}
