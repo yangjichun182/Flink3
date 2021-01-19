657549646844c2fff5af8c0aeac108990fec82e9
yangjichun182 <390835164@qq.com>
2021-01-19 22:18:20 +0800

    Utils包下的文件列表说明

--- src/main/scala/org/example/utils/UtilsIllustrate.java
+++ src/main/scala/org/example/utils/UtilsIllustrate.java
@@ -1,4 +1,15 @@
 package org.example.utils;
 
+//Java的IO技术，文件的读写操作
+import org.example.utils.FileUtils;
+/**
+ * @author : chunji.yang
+ * @date : 2021-01-19 22:05
+ * @remark : Utils包下的文件列表说明
+ */
 public class UtilsIllustrate {
+    public static void main(String[] args) {
+
+    }
+
 }
