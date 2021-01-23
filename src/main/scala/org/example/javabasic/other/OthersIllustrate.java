91e1b12170451d2978dca7ab5a6341dbc35355d0
yangjichun182 <390835164@qq.com>
2021-01-23 22:28:17 +0800

    用java模拟堆栈

--- src/main/scala/org/example/javabasic/other/OthersIllustrate.java
+++ src/main/scala/org/example/javabasic/other/OthersIllustrate.java
@@ -1,4 +1,17 @@
 package org.example.javabasic.other;
+//Java模拟堆栈
 
+import org.example.javabasic.other.Stack202101232147;
+//获取数组的最大值和最小值
+import org.example.javabasic.other.ArrayMax202101122048;
+//实现将数据写到文件里面
+import org.example.javabasic.other.FileRead202101112149;
+
+/**
+ * @author : chunji.yang
+ * @date : 2021-01-23 21:48
+ * @remark : others包下类总结
+ */
 public class OthersIllustrate {
+
 }
--- src/main/scala/org/example/javabasic/other/Stack202101232147.java
+++ src/main/scala/org/example/javabasic/other/Stack202101232147.java
@@ -1,8 +1,67 @@
-package org.example.javabasic;
+package org.example.javabasic.other;
+
 /**
  * @author : chunji.yang
  * @date : 2021-01-23 21:48
  * @remark : Java模拟堆栈
  */
 public class Stack202101232147 {
+    /**
+     * 存储区域
+     */
+    int[] data;
+    /**
+     * 最大存储量
+     */
+    int maxSize;
+    int top;
+    //constructor
+    public Stack202101232147(int maxSize) {
+        this.maxSize = maxSize;
+        this.data = new int[maxSize];
+        top = -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Stack202101232147 stack = new Stack202101232147(1000);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        while (stack.top >= 0) {
+            System.out.println(stack.pop());
+        }
+    }
+
+
+    /**
+     * 将数据放入堆栈
+     *
+     * @param data
+     * @return
+     */
+    public boolean push(int data) {
+        if (top + 1 == maxSize) {
+            System.out.println("队站已满");
+            return false;
+        }
+        this.data[++top] = data;
+        return true;
+    }
+
+    /**
+     * 将数据取出堆栈
+     *
+     * @return
+     * @throws Exception
+     */
+    public int pop() throws Exception {
+        if (top == -1) {
+            throw new Exception("堆栈已空");
+        }
+        return this.data[top--];
+    }
+
+
 }
